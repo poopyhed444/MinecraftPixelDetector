3f895698bacbba8acea27d03f41bfd5cdca2e374
xinepeng <smartpeoplelosesp@gmail.com>
2023-07-06 13:05:56 -0400

    partially working?
    gotta fix this tho: org.bukkit.command.CommandException: Unhandled exception executing command 'skindetect' in plugin SkinDetector v4.2.0
                                at org.bukkit.command.PluginCommand.execute(PluginCommand.java:47) ~[patched_1.8.8.jar:git-PandaSpigot-110]

--- src/main/java/org/xinepeng/Plugin.java
+++ src/main/java/org/xinepeng/Plugin.java
@@ -1,13 +1,17 @@
 package org.xinepeng;
 
 import org.bukkit.plugin.java.JavaPlugin;
-public class Plugin extends JavaPlugin{
+public class Plugin extends JavaPlugin {
+  @Override
+
   public void onEnable() {
     this.getCommand("skindetect").setExecutor(new SkinDetector());
     getLogger().info("SkinDetector has been enabled!");
 
     }
-    public void onDisable() {
+  @Override
+
+  public void onDisable() {
     getLogger().info("SkinDetector has been disabled!");
 
     }
--- src/main/java/org/xinepeng/SkinDetector.java
+++ src/main/java/org/xinepeng/SkinDetector.java
@@ -1,8 +1,11 @@
 package org.xinepeng;
+
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+
 import java.io.IOException;
+
 import static org.xinepeng.OpenCV.ColorDetector;
 import static org.xinepeng.SkinRequest.UUIDGrabber;
 public class SkinDetector implements CommandExecutor {
@@ -11,7 +14,7 @@ public class SkinDetector implements CommandExecutor {
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
         if (commandSender.isOp()) {
             try {
-                ColorDetector(UUIDGrabber(strings[0]));
+                commandSender.sendMessage(ColorDetector(UUIDGrabber(strings[0])));
                 return true;
             } catch (IOException e) {
                 commandSender.sendMessage("§c§l[SkinDetector] §c§lError: §c§l" + e.getMessage());
