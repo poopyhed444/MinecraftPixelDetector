6664f22c00b0e9ea01e92d0fad2c87169a88e5d2
xinepeng <smartpeoplelosesp@gmail.com>
2023-07-06 12:30:44 -0400

    made color detector more accurate (i think)

--- src/main/java/org/xinepeng/SkinDetector.java
+++ src/main/java/org/xinepeng/SkinDetector.java
@@ -1,20 +1,17 @@
 package org.xinepeng;
-
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-
 import java.io.IOException;
-
+import static org.xinepeng.OpenCV.ColorDetector;
 import static org.xinepeng.SkinRequest.UUIDGrabber;
-
 public class SkinDetector implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
         if (commandSender.isOp()) {
             try {
-                UUIDGrabber(strings[0]);
+                ColorDetector(UUIDGrabber(strings[0]));
                 return true;
             } catch (IOException e) {
                 commandSender.sendMessage("§c§l[SkinDetector] §c§lError: §c§l" + e.getMessage());
